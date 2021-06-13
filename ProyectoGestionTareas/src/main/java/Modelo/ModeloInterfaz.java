3a913e8ff4b01badc9c4f28f4b67fe565a1de3bf
meGustaLaPaella <78791173+meGustaLaPaella@users.noreply.github.com>
2021-06-13 18:45:48 +0200

    Interfaz modelo ceada

--- ProyectoGestionTareas/src/main/java/Modelo/ModeloInterfaz.java
+++ ProyectoGestionTareas/src/main/java/Modelo/ModeloInterfaz.java
@@ -1,5 +1,60 @@
 package Modelo;
 
-public interface ModeloInterfaz {
+import Clases.Persona;
+import Clases.Proyecto;
+import Clases.Tarea;
+import Excepciones.ListaVaciaException;
+import Excepciones.PersonaRepetidaException;
+import Excepciones.TareaRepetidaException;
+import Resultados.Resultado;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+public interface ModeloInterfaz {
+    public ArrayList<Tarea> getListTareas();
+    public ArrayList<Persona> getListaPersonas();
+    public void darDeAltaPersona(String dni, String nombre, String correo) throws ListaVaciaException, PersonaRepetidaException;
+    public String nombreTarea(int index);
+    public void borrarPersona(int index);
+    public Persona getUltimaPersona();
+    public void darDeAltaTarea(String titulo, String descripcion, String responsable, int prioridad, double coste, int facturacion,double var, int resultado, String id, int horas, int tipo  ) throws TareaRepetidaException;
+    public Tarea buscarTarea(String titulo);
+    public void setProyecto(Proyecto proyecto);
+    public String getNombreProyecto();
+    public Proyecto getProyecto();
+    public boolean isCerrarPrograma();
+    public  String leerString();
+    public  int leerInt();
+    public Double leerDouble();
+    public  Proyecto cargarProyecto() throws IOException, ClassNotFoundException;
+    public void guardarProyecto() throws IOException;
+    public void pedirNombreTarea();
+    public void nombresTareas();
+    public void anyadirPersonaATarea(Tarea t, Persona p);
+    public void anyadirTareaAPersona(Persona p, Tarea t);
+    public void setTareaFinalizada(int index);
+    public Tarea getUltimaTarea();
+    public boolean sePuedeAnyadir(Persona p);
+    public boolean sePuedeAnyadir(Tarea t);
+    public void eliminarPersonaDeTarea(Tarea t, int index);
+    public String getResultado(int index);
+    public void addResultados(String s);
+    public void quitarResultado(int index);
+    public ArrayList<Persona> getPersonasDeUnaTarea(Tarea t);
+    public ArrayList<Tarea> getTareasDeUnaPersona(Persona p);
+    public ArrayList<Persona> getPersonasDeUnaTarea(int index);
+    public ArrayList<Tarea> getTareasDeUnaPersona(int index);
+    public void eliminarTareaDePersona(Persona p, int index);
+    public void setResultado(Resultado r, Tarea t);
+    public Tarea getTarea(int index);
+    public Persona getPersona(int index);
+    public String getID(int index);
+    public String getTipoTarea(int index);
+    public String getDescripcion(int index);
+    public Date getFechaCreacion(int index);
+    public String getResponsable(int index);
+    public void addEtiqueta(String s, Tarea t);
+    public String getEtiquetas(int index);
 }
